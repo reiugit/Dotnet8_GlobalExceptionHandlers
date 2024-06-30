fc35fa147afb5cfbc20bd803e6639066b01ea374
rei <67372053+reiugit@users.noreply.github.com>
2024-06-30 08:05:26 +0200

    Added comment for return value

--- Dotnet8_GlobalExceptionHandlers/ExceptionHandlers/OnlyLoggingExceptionHandler.cs
+++ Dotnet8_GlobalExceptionHandlers/ExceptionHandlers/OnlyLoggingExceptionHandler.cs
@@ -6,10 +6,12 @@ namespace Dotnet8_GlobalExceptionHandlers.ExceptionHandlers
     {
         public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
-            logger.LogInformation("An exception was intentionally invoked:  StatusCode {StatusCode}\n      {message}",
+            logger.LogInformation("An exception was intentionally invoked:  StatusCode {StatusCode}\n      {message}\n",
                                          httpContext.Response.StatusCode,
                                          exception.Message);
 
+            // return false to indicate that the exception was not handled
+            // and the next exception handler in the pipeline should be invoked
             return ValueTask.FromResult(false);
         }
     }
